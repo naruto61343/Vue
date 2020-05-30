deae7d974e700a3cda30e81b62734b64107c032a
Cathy <naruto61343@gmail.com>
2020-05-31 00:34:47 +0800

    [Add] src file

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <div id="app">   
+      <div class="col-xs-12">
+                <app-user></app-user>
+            </div>
+    </div>
+  
+</template>
+
+<script>
+ import User from './components/User.vue';
+
+export default {
+  name: 'App',
+  components: {
+    appUser: User
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
