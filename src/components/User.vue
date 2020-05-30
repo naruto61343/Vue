80ccbfa961d1d64b80b2355ed9cbdb15cbc5fdb7
Cathy <naruto61343@gmail.com>
2020-05-31 00:16:36 +0800

    Initial commit

---
+++ src/components/User.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an Parent</p>
+        <button @click="changeName">Change my Name</button>
+        <p>Name is {{ name }}</p>
+        <p>Age is {{ age }}</p>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail
+                        :myName="name"
+                        @nameWasReset="name = $event"
+                        :resetFn="resetName"
+                        :userAge="age"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit
+                        :userAge="age"
+                        @ageWasEdited="age = $event"
+                ></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function () {
+            return {
+                name: 'Cat',
+                age: 27
+            };
+        },
+        methods: {
+            changeName() {
+                this.name = 'blackCat';
+            },
+            resetName() {
+                this.name = 'Cat';
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style>
+    div {
+        background-color: lightblue;
+    }
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+
+export const eventBus = new Vue({
+  methods: {
+      changeAge(age) {
+          this.$emit('ageWasEdited', age);
+      }
+  }
+});
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
